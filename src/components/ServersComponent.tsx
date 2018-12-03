30272ac00d8e77dc8c63d3f904894c3930f35400
Debsmita <debsmita.santra@gmail.com>
2018-12-03 23:01:53 +0530

    Changes in the Login Component For url-s che.openshift.io and che.prod-preview.openshift.io no username password would be asked, it should be triggered the OISO way. For any url other than the above mentioned url-s the user will have to authenticate himself/herself

--- src/components/ServersComponent.tsx
+++ src/components/ServersComponent.tsx
@@ -32,7 +32,7 @@ class ServersComponent extends React.Component<IProps> {
         if(this.props.Pservers !== null)
         {
             for (const key in this.props.Pservers) {
-            if (this.props.Pservers.hasOwnProperty(key) && this.props.Pservers[key] !== "") {
+            if ((this.props.Pservers[key] !== "") && this.props.Pservers.hasOwnProperty(key)) {
                 servers.push(
                     <ServerComponent PsetCurrentWorkspace ={ this.props.PsetCurrentWorkspace} Pservers={this.props.Pservers} server={this.props.Pservers[key]} key={key} PsetCurrentServer={this.props.PsetCurrentServer} PcurrentServer = {this.props.PcurrentServer}
                     PupdateWorkspacesList = {this.props.PupdateWorkspacesList}/>
